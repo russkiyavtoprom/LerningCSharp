3c6fce6a86ca5964d0ea84fe416318a77bc5f635
russkiyavtoprom <84123329+russkiyavtoprom@users.noreply.github.com>
2022-03-15 19:59:14 +0300

    ttttt
    tttt

---
+++ Drawing.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Risovanie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            int pos = Console.CursorTop;
+            Console.SetCursorPosition(50, 20);
+            Console.SetCursorPosition(10, pos);
+
+            var rand = new Random();
+
+            for (int i = 0; i <= 100; i++)
+            {
+                string pct = string.Format( new string((char)0x2592, i * 30 / 100), i);
+                Console.CursorLeft = 0;
+                Console.Write(pct);
+                Thread.Sleep(rand.Next(0, 50));
+            }
+            Console.WriteLine();
+            Console.ResetColor();
+        }
+
+        static void Print(string message, int delay)
+        {
+            Thread.Sleep(delay);
+            Console.WriteLine(message);
+        }
+    }
+}
